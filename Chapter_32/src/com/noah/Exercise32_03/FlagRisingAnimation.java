01677f48890895ac5e54ad982da39cb4689435fe
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2023-05-23 13:16:41 -0600

    fixed invalid uri

--- Chapter_32/src/com/noah/Exercise32_03/FlagRisingAnimation.java
+++ Chapter_32/src/com/noah/Exercise32_03/FlagRisingAnimation.java
@@ -1,8 +1,11 @@
 package com.noah.Exercise32_03;
 
+import java.io.File;
+
 import javafx.animation.PathTransition; 
 import javafx.application.Application; 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
@@ -16,12 +19,16 @@ public class FlagRisingAnimation extends Application {
 		Pane pane = new Pane();
 	
 		// Add an image view and add it to pane
-		ImageView imageView = new ImageView("file:image/us.gif");
+		ImageView imageView = new ImageView("us.gif");
+		
 		pane.getChildren().add(imageView);
 
 		// Create a path transition
-		PathTransition pt = new PathTransition(Duration.millis(10000), new Line(100, 200, 100, 0), imageView); pt.setCycleCount(5);
+		PathTransition pt = new PathTransition(Duration.millis(10000), new Line(100, 200, 100, 0), imageView); 
+		pt.setCycleCount(5);
 		pt.play(); // Start animation
+		
+		///
 		
 		// Create a scene and place it in the stage
 		Scene scene = new Scene(pane, 250, 200); 
